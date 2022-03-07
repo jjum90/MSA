f67ee3b663a6699a688e2a77421f5438aa0027b3
minseok <jjum90@naver.com>
2022-03-08 00:45:06 +0900

    remove file

--- Unentered/src/main/java/com/microservice/unentered/UnenteredRepository.java
+++
@@ -1,7 +0,0 @@
-package com.microservice.unentered;
-
-import com.microservice.unentered.entity.UnenteredCheckHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UnenteredRepository extends JpaRepository <UnenteredCheckHistory, Integer> {
-}
