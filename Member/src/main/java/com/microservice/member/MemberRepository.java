2dcd6b745224622505d0bc9b1872ac0eef2c4463
minseok <jjum90@naver.com>
2022-03-08 00:43:36 +0900

    remove file

--- Member/src/main/java/com/microservice/member/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.microservice.member;
-
-import com.microservice.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Integer> {
-}
