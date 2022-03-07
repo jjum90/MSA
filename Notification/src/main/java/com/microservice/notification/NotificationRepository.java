9b0a200bfbc558ae90a2290aaabc190a51431695
minseok <jjum90@naver.com>
2022-03-08 00:44:25 +0900

    remove file

--- Notification/src/main/java/com/microservice/notification/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.microservice.notification;
-
-import com.microservice.notification.entity.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Integer> {
-}
