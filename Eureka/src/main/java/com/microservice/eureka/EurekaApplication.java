aa4d0e5d5b8b3658263434732d5c47ce97b17be2
minseok <jjum90@naver.com>
2022-03-08 00:22:06 +0900

    API Gateway 모듈 추가

--- Eureka/src/main/java/com/microservice/eureka/EurekaApplication.java
+++ Eureka/src/main/java/com/microservice/eureka/EurekaApplication.java
@@ -6,6 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+/**
+ * Eureka 서버
+ */
 public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
---
+++ api-gateway/src/main/java/com/microservice/apigw/ApiGateWayApplication.java
@@ -0,0 +1,19 @@
+package com.microservice.apigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+/**
+ * APIGATEWAY
+ * 프록시의 역할과 로드밸런싱
+ * 인증 및 로깅 서버로서의 기능
+ * 클라이언트로부터 모든 요청을 받아 인증, 로깅 절차를 거칠 수 있고 모든 마이크로서비스를 호출 가능
+ */
+public class ApiGateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateWayApplication.class, args);
+    }
+}
